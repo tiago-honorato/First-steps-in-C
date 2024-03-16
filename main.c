6618595a32dfb39911d42b8c21c5fe988279bf13
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-16 02:28:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//declarações das funções
 int somar(int a, int b);
+int converterParaDolar(int valor);
 
+//função principal
 int main() {
 	
-	int result = somar(10, 10);
+	//declarações das variáveis
+	int result, valor;
+	//chamada da função somar();
+	result = somar(10, 10);
 	
-	printf("salve!");
+	printf("Programa em C\n");
 	
 	printf("%d", result);
 	
+	valor = converterParaDolar(10);
+	
+	printf("\n%d", valor);
+	
+	//retorno da função
 	return 0;
 }
 
+//definição da função
 int somar(int a, int b){
 	
+	//retorno da função
 	return a+b;
 	
 }
+
+//definição da função
+int converterParaDolar(int valor){
+	
+	//retorno da função
+	return valor/5;
+	
+}
+
+
