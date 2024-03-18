a10bf739b27f58665cc0e91a0d5b0e966dd3ba8f
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-18 03:12:13 -0300

    Add files via upload

---
+++ BooleanUsandoDiretiva.c
@@ -0,0 +1,21 @@
+//bibliotecas
+#include <stdio.h>
+
+//define que todo valor de TRUE é igual a 1
+#define TRUE 1
+//define que todo valor de FALSE é igual a 0
+#define FALSE 0
+
+int main(){
+
+	/*
+	Em C, o tipo de dado mais próximo de um booleano é int,
+	onde zero (0) é considerado falso e qualquer outro valor é considerado verdadeiro.
+	No entanto, para maior clareza e legibilidade, é comum usar a diretiva #define
+	*/
+	
+	int boolean = TRUE;
+	int boolean2 = FALSE;
+	
+	return 0;
+}
