5fc287a11a38901c26dce75db357421a8085cb3e
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-18 03:01:44 -0300

    Update TiposDeDados.c

--- TiposDeDados.c
+++ TiposDeDados.c
@@ -1,15 +1,22 @@
 //bibliotecas
 #include <stdio.h>
 
+//função principal
 int main(){
-	
+
+	//tipo inteiro
 	int inteiro = 10;
+	//tipo usado para representar números decimais com precisão simples. 
 	float numeroComVirgula = 2.10;
+	//tipo usado para representar números decimais com maior precisão.
 	double dupla_precisao = 3.141592653589;
-	char caractere = "A";
+	//tipo usado para representar caracteres individuais.
+	char caractere = 'A';
+
+	//uma constante(const) é um valor que não muda. Geralmente é escrito em uppercase.
+	const int VALOR_PI = 3.14159;
 	
-	//Em C não existe String nem Boolean.
-	
+	//Em C não existe os tipos String nem Boolean.
 
 	return 0;
 }
