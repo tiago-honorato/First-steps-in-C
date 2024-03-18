9a777606ccd78471125e82994ba7205b095b643a
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-18 02:49:19 -0300

    Add files via upload

---
+++ TiposDeDados.c
@@ -0,0 +1,15 @@
+//bibliotecas
+#include <stdio.h>
+
+int main(){
+	
+	int inteiro = 10;
+	float numeroComVirgula = 2.10;
+	double dupla_precisao = 3.141592653589;
+	char caractere = "A";
+	
+	//Em C não existe String nem Boolean.
+	
+
+	return 0;
+}
