7f664194c8c8476eff587a1c06c1a422c81f2818
Tiago Honorato <85461392+tiago-honorato@users.noreply.github.com>
2024-03-18 12:37:08 -0300

    Update EstruturaCondicional.c

--- EstruturaCondicional.c
+++ EstruturaCondicional.c
@@ -3,7 +3,7 @@
 int main() {
 	
     int idade = 23;
-
+    //Condicional IF
     if (idade >= 18) {
     	
         printf("Você é maior de idade.\n");
@@ -17,6 +17,15 @@ int main() {
         printf("Você é criança.\n");
         
     }
+    //operador Ternário
+    int a = 10;
+    int b = 5;
+    int maior;
+
+    // Verifica qual é o maior número entre a e b
+    maior = (a > b) ? a : b;
+
+    printf("O maior número é: %d\n", maior);
 
     return 0;
 }
